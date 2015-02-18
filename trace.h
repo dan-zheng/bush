9367a75f6b028e07617bcfeb5ad1b4da743e39c3
jluchiji <jluchiji@purdue.edu>
2015-02-18 11:07:46 -0500

    Removed unused macros from trace.h

--- trace.h
+++ trace.h
@@ -1,21 +1,9 @@
+
+
 #ifndef TRACE_H
 #define TRACE_H
 
 #include <stdio.h>
-
-/*
-Black       0;30     Dark Gray     1;30
-Blue        0;34     Light Blue    1;34
-Green       0;32     Light Green   1;32
-Cyan        0;36     Light Cyan    1;36
-Red         0;31     Light Red     1;31
-Purple      0;35     Light Purple  1;35
-Brown       0;33     Yellow        1;33
-Light Gray  0;37     White         1;37
-*/
-
-#define FIRST_ARG_(N, ...) N
-#define FIRST_ARG(args) FIRST_ARG_ args
 
 #ifndef NO_COLORS
 
